1afb157c74fd5a6ac5c38ff55cb86b04e364d68c
refojunior <refojunior5@gmail.com>
2018-01-01 22:08:49 +0800

    komit terbaru

---
+++ blog.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="image" content="img/logo.png">
+	<meta name="description" content="7 Bakery adalah toko roti terbesar di Bali, yang menyediakan banyak pilihan roti yang berkualitas dan harga terjangkau">
+	<title>Blog - 7 Bakery</title>
+	<link rel="icon" href="img/logo.png" type="image"> 
+	<!-- LOAD STYLE FROM BOOTSTRAP LIBRARY-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<!-- LOAD STYLE MANUAL -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="prettyPhoto/css/prettyPhoto.css" media="screen" charset="utf-8" />
+	<!-- LOAD JAVASCRIPT FROM BOOTSTRAP LIBRARY -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="prettyPhoto/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+	<script type="text/javascript" charset="utf-8">
+	  $(document).ready(function(){
+	    $("a[rel^='prettyPhoto']").prettyPhoto();
+	  });
+	</script>
+</head>
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-md navbar-light fixed-top" style="background:#fdf58f" >
+      <a class="navbar-brand" href="#">
+      	<img src="img/logo.png" width="30" height="40" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        
+        <ul class="navbar-nav mr-auto ">
+          <!-- SENGAJA DIKOSONGKAN AGAR NAVBAR BISA BERADA DIPOSISI KANAN -->
+        </ul>	
+        <ul class="navbar-nav" id="scrollWeb">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php#service">Service</a>
+          </li>
+          <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Product
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="product.php">Lihat Semua Product</a>
+	          <div class="dropdown-divider"></div>
+	          <a class="dropdown-item" href="product/roti-isi.php">Roti Isi</a>
+	          <a class="dropdown-item" href="product/sandwich.php">Sandwich</a>
+	          <a class="dropdown-item" href="product/donat.php">Donat</a>
+	          <a class="dropdown-item" href="product/brownies.php">Brownies</a>
+	          <a class="dropdown-item" href="product/kue-basah.php">Kue Basah</a>
+	          <a class="dropdown-item" href="product/kue-kering.php">Kue Kering</a>
+	          <a class="dropdown-item" href="product/kue-tart.php">Kue Tart</a>
+	          <a class="dropdown-item" href="product/pudding.php">Pudding</a>
+	        </div>
+	      </li>
+          <li class="nav-item">
+            <a class="nav-link" href="gallery.php">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="blog.php">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.php">Contact Us</a>
+          </li>
+        </ul>	
+      </div>
+    </nav>
+    <br><br>
+ <!-- end navbar -->
+ 	<div class="container-fluid">
+ 		<div class="row">
+			<img src="img/page-blog.jpg" alt="" class="img-fluid img-header">
+			<div class="centered">Blog</div>
+		</div>
+		<br>
+		<h1 align="center">Blog Terbaru</h1>
+		<hr>
+		<div class="row">
+			<div class="col-md-8 col-lg-8 col-xs-8">
+				<div class="media">
+				  <img class="mr-3 img-blog" src="img/blog-1.jpg" alt="Generic placeholder image">
+				  <div class="media-body">
+				    <h5 class="mt-0">Tips Memilih Bahan Roti Berkualitas</h5>
+				    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				    quis nostrud exercitation...
+				    <br><br>
+				    <p><a href="blog/tips-memilih-bahan-roti-berkualitas.php" class="btn btn-primary">Read More</a></p>
+				  </div>
+				</div>
+				<div class="media">
+				  <img class="mr-3 img-blog" src="img/blog-2.jpg" alt="Generic placeholder image">
+				  <div class="media-body">
+				    <h5 class="mt-0">Roti yang Baik Untuk Kesehatan</h5>
+				    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				    quis nostrud exercitation...
+				    <br><br>
+				    <p><a href="#" class="btn btn-primary">Read More</a></p>
+				  </div>
+				</div>
+				<div class="media">
+				  <img class="mr-3 img-blog" src="img/blog-3.jpg" alt="Generic placeholder image">
+				  <div class="media-body">
+				    <h5 class="mt-0">Hidup Sehat ala Mr. John</h5>
+				    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				    quis nostrud exercitation...
+				    <br><br>
+				    <p><a href="#" class="btn btn-primary">Read More</a></p>
+				  </div>
+				</div>
+				<div class="media">
+				  <img class="mr-3 img-blog" src="img/blog-4.png" alt="Generic placeholder image">
+				  <div class="media-body">
+				    <h5 class="mt-0">Resep Rahasia Sandwich Enak</h5>
+				    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				    quis nostrud exercitation...
+				    <br><br>
+				    <p><a href="#" class="btn btn-primary">Read More</a></p>
+				  </div>
+				</div>
+			</div>
+
+			<div class="col-md-4 col-xs-4 col-lg-4">
+				<h2>Search</h2>
+				<input type="text" class="form-control" name="">
+				<a href="#" class="btn btn-secondary"> Cari</a>
+				<br><br>
+				<div class="list-group">
+				  <a href="#" class="list-group-item list-group-item-warning active">
+				    Blog Terpopuler
+				  </a>
+				  <a href="#" class="list-group-item list-group-item-action">Roti yang Baik Untuk Kesehatan</a>
+				  <a href="#" class="list-group-item list-group-item-action">Tips Memilih Bahan Roti Berkualitas</a>
+				  <a href="#" class="list-group-item list-group-item-action">Hidup Sehat ala Mr. John</a>
+				  <a href="#" class="list-group-item list-group-item-action">Resep Rahasia Sandwich Enak</a>
+				</div>
+			</div>
+		</div>	
+		<br><br>
+		<div class="row" id="contact">
+		<img src="img/contact.jpg" alt="bg-img" width="100%" height="100%">
+		<div class="col-lg-12 col-md-12 col-xs-12 absolute" >
+		      <br>
+		      <h1>Contact Us</h1>  
+		      <hr>
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-xs-6">
+					<h2>Get In Touch : </h2>
+					<div class="address">
+                        <div class="address-grid">
+                            <div class="address-left">
+                                <span class="fa fa-phone" aria-hidden="true"></span>
+                            </div>
+                            <div class="address-right">
+                                <h6>Phone Number</h6>
+                                <p>+6283114738768</p>
+                            </div>
+                            <br><br>
+                        </div>
+					</div>
+					<div class="address">
+                        <div class="address-grid">
+                            <div class="address-left">
+                                <span class="fa fa-envelope" aria-hidden="true"></span>
+                            </div>
+                            <div class="address-right">
+                                <h6>Email</h6>
+                                <p>info@7bakery.com</p>
+                            </div>
+                            <br><br>
+                        </div>
+					</div>
+					<div class="address">
+                        <div class="address-grid">
+                            <div class="address-left">
+                                <span class="fa fa-map-marker" aria-hidden="true"></span>
+                            </div>
+                            <div class="address-right">
+                                <h6>Location</h6>
+                                <p>Jl. Teuku Umar No. 1</p>
+                            </div>
+                            <br><br><br>
+                        </div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-xs-6 bg-contact">
+					<div class="contact-form">
+						<h2 style="font-weight:bold">Contact Form : </h2>
+						Nama Lengkap : 
+						<input type="text" class="form-control">
+						Email : 
+						<input type="text" class="form-control">
+						Subject : 
+						<input type="text" class="form-control">
+						Pesan : 
+						<textarea class="form-control"></textarea>
+						<br>
+						<input type="submit" class="btn btn-lg btn-primary" value="Kirim">
+						<br>
+						<br>
+					</div>
+				</div>
+			</div>
+			<br>
+			<!-- footer -->
+			<div class="row">
+				<div class="footer " >
+				&copy; 7 Bakery 2017
+				</div>
+			</div>	
+		</div>	
+	</div>
+ 	</div>
+
+
+	<script type="text/javascript" src="js/jquery.flex-images.min.js"></script>
+	<script type="text/javascript">
+	    $(".galery").flexImages({rowHeight:350});
+	</script>
+</body>
+</html>
---
+++ blog/tips-memilih-bahan-roti-berkualitas.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="image" content="../img/logo.png">
+	<meta name="description" content="7 Bakery adalah toko roti terbesar di Bali, yang menyediakan banyak pilihan roti yang berkualitas dan harga terjangkau">
+	<title>Tips Memilih Bahan Roti Berkualitas - 7 Bakery</title>
+	<link rel="icon" href="../img/logo.png" type="image"> 
+	<!-- LOAD STYLE FROM BOOTSTRAP LIBRARY-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<!-- LOAD STYLE MANUAL -->
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../font-awesome/css/font-awesome.css">
+	<!--UNITE GALLERY -->
+	
+	<!-- LOAD JAVASCRIPT FROM BOOTSTRAP LIBRARY -->
+	<script src="../js/jquery-3.2.1.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script src="../prettyPhoto/js/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+	<script type="text/javascript" charset="utf-8">
+	  $(document).ready(function(){
+	    $("a[rel^='prettyPhoto']").prettyPhoto();
+	  });
+	</script>
+</head>
+<body>
+	<nav class="navbar navbar-expand-md navbar-light fixed-top" style="background:#fdf58f" >
+      <a class="navbar-brand" href="#">
+      	<img src="../img/logo.png" width="30" height="40" alt="">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        
+        <ul class="navbar-nav mr-auto ">
+          <!-- SENGAJA DIKOSONGKAN AGAR NAVBAR BISA BERADA DIPOSISI KANAN -->
+        </ul>	
+        <ul class="navbar-nav" id="scrollWeb">
+          <li class="nav-item">
+            <a class="nav-link" href="../index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="../about.php">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../index.php#service">Service</a>
+          </li>
+          <li class="nav-item dropdown">
+	        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	          Product
+	        </a>
+	        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          <a class="dropdown-item" href="../product.php">Lihat Semua Product</a>
+	          <div class="dropdown-divider"></div>
+	          <a class="dropdown-item" href="roti-isi.php">Roti Isi</a>
+	          <a class="dropdown-item" href="sandwich.php">Sandwich</a>
+	          <a class="dropdown-item" href="donat.php">Donat</a>
+	          <a class="dropdown-item" href="brownies.php">Brownies</a>
+	          <a class="dropdown-item" href="kue-basah.php">Kue Basah</a>
+	          <a class="dropdown-item" href="kue-kering.php">Kue Kering</a>
+	          <a class="dropdown-item" href="kue-tart.php">Kue Tart</a>
+	          <a class="dropdown-item" href="pudding.php">Pudding</a>
+	        </div>
+	      </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../gallery.php">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../blog.php">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../contact.php">Contact Us</a>
+          </li>
+        </ul>	
+      </div>
+    </nav>
+    <br><br>
+
+    <div class="container-fluid">
+    	<div class="row">
+			<img src="../img/page-blog.jpg" alt="" class="img-fluid img-header">
+			<div class="centered">Blog</div>
+		</div>
+		<br>
+
+		<h1 align="left" style="margin-left:20px">Tips Memilih Bahan Roti Berkualitas</h1>
+		<hr>
+		<div class="row" style="margin-left:20px">
+			<div class="col-md-8 col-lg-8 col-xs-8 blog">
+				
+				<img src="../img/blog-1.jpg" class="img-responsive">
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="col-md-4 col-xs-4 col-lg-4">
+				<h2>Search</h2>
+				<input type="text" class="form-control" name="">
+				<a href="#" class="btn btn-secondary"> Cari</a>
+				<br><br>
+				<div class="list-group">
+				  <a href="#" class="list-group-item list-group-item-warning active">
+				    Blog Terpopuler
+				  </a>
+				  <a href="#" class="list-group-item list-group-item-action">Roti yang Baik Untuk Kesehatan</a>
+				  <a href="#" class="list-group-item list-group-item-action">Tips Memilih Bahan Roti Berkualitas</a>
+				  <a href="#" class="list-group-item list-group-item-action">Hidup Sehat ala Mr. John</a>
+				  <a href="#" class="list-group-item list-group-item-action">Resep Rahasia Sandwich Enak</a>
+				</div>
+			</div>
+		</div>
+		<br><br>
+		<div class="row" id="contact">
+			<img src="../img/contact.jpg" alt="bg-img" width="100%" height="100%">
+			<div class="col-lg-12 col-md-12 col-xs-12 absolute" >
+			      <br>
+			      <h1>Contact Us</h1>  
+			      <hr>
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-xs-6">
+						<h2>Get In Touch : </h2>
+						<div class="address">
+	                        <div class="address-grid">
+	                            <div class="address-left">
+	                                <span class="fa fa-phone" aria-hidden="true"></span>
+	                            </div>
+	                            <div class="address-right">
+	                                <h6>Phone Number</h6>
+	                                <p>+6283114738768</p>
+	                            </div>
+	                            <br><br>
+	                        </div>
+						</div>
+						<div class="address">
+	                        <div class="address-grid">
+	                            <div class="address-left">
+	                                <span class="fa fa-envelope" aria-hidden="true"></span>
+	                            </div>
+	                            <div class="address-right">
+	                                <h6>Email</h6>
+	                                <p>info@7bakery.com</p>
+	                            </div>
+	                            <br><br>
+	                        </div>
+						</div>
+						<div class="address">
+	                        <div class="address-grid">
+	                            <div class="address-left">
+	                                <span class="fa fa-map-marker" aria-hidden="true"></span>
+	                            </div>
+	                            <div class="address-right">
+	                                <h6>Location</h6>
+	                                <p>Jl. Teuku Umar No. 1</p>
+	                            </div>
+	                            <br><br><br>
+	                        </div>
+						</div>
+					</div>
+
+					<div class="col-lg-6 col-md-6 col-xs-6 bg-contact">
+						<div class="contact-form">
+							<h2 style="font-weight:bold">Contact Form : </h2>
+							Nama Lengkap : 
+							<input type="text" class="form-control">
+							Email : 
+							<input type="text" class="form-control">
+							Subject : 
+							<input type="text" class="form-control">
+							Pesan : 
+							<textarea class="form-control"></textarea>
+							<br>
+							<input type="submit" class="btn btn-lg btn-primary" value="Kirim">
+							<br>
+							<br>
+						</div>
+					</div>
+				</div>	
+				<br>
+				<div class="row">
+					<div class="footer">
+					&copy; 7 Bakery 2017
+					</div>
+				</div>
+			</div>	
+		</div>
+
+    </div>
+
+</body>
+</html>
